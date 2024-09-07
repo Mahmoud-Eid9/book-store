bc1af8ab9e2309e6d1d396e026848de2e70f705a
Mahmoud Eid <mahmoudeid947@gmail.com>
2024-09-07 17:59:16 +0300

    first commit

--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mainRouter = require('./routes/mainRouter');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mainRouter = require("./routes/mainRouter");
 const app = express();
-const pool = require('./config/db_config');
-
+const pool = require("./config/db_config");
 
 app.use(cors());
 app.use(express.json());
 app.use(mainRouter);
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-
 app.listen(8000, async () => {
-    console.log("listening on 8000.....")
+  console.log("listening on 8000.....");
 });
